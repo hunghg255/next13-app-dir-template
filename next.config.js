736500506124460a082607b0ad0fae606996df8e
hunghg255 <giahunghust@gmail.com>
2023-05-11 17:37:06 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
+const path = require('path');
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
 
 module.exports = withBundleAnalyzer(nextConfig);
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Hello from '@components/Hello';
-import styles from './page.module.css';
+import styles from './page.module.scss';
 
 export default function Home() {
   return (
