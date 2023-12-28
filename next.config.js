cdbaaf3241c3d55fb8c9cd88e4f6808a1ba4e21e
hunghg255 <giahunghust@gmail.com>
2023-12-28 11:04:53 +0700

    chore: update

--- next.config.js
+++ next.config.js
@@ -6,11 +6,52 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 const isProd = process.env.NODE_ENV === 'production';
 
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  headers: async function headers() {
+    if (process.env.NODE_ENV === 'development') {
+      return [];
+    }
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+      {
+        source: '/:all*(svg|jpg|jpeg|png|webp|avif|otf|ttf|woff|woff2|eot)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=9999999999, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
+  poweredByHeader: false,
 };
 
 module.exports = isProd ? nextConfig : withBundleAnalyzer(nextConfig);
